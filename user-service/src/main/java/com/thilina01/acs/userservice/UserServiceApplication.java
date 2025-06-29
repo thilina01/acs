471ea26dd1b0b3952ee167648d21aad23ce11f05
thilina01 <r.thilina@gmail.com>
2025-06-29 17:08:40 +0530

    user service initial version

---
+++ user-service/src/main/java/com/thilina01/acs/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thilina01.acs.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/thilina01/acs/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilina01.acs.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
