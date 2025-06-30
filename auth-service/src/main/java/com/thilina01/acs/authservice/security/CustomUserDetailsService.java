8f803afc011c91615b60ecf6d87323054363c065
thilina01 <r.thilina@gmail.com>
2025-06-30 18:38:23 +0530

    Docker health checks / resilience for Redis/DB failure recovery.

--- auth-service/src/main/java/com/thilina01/acs/authservice/security/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/thilina01/acs/authservice/security/CustomUserDetailsService.java
@@ -28,8 +28,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getUsername())
-                .password(user.getPassword()) // make sure it's encoded!
-                .roles(user.getRole())        // converts to ROLE_{role}
+                .password(user.getPassword())
+                .roles(user.getRole())
                 .build();
     }
 }
